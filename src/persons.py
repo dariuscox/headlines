7fe8a31ed2224ab85750f69d518e122d5e034c7a
Darius Cox <ddcgamer243@gmail.com>
2018-04-25 21:36:29 -0400

    can check order of 2 cards

--- src/persons.py
+++ src/persons.py
@@ -25,15 +25,55 @@ def shuffle_events(events):
     shuffle(names)
     return names
 
+def give_card(deck,events):
+    card = deck.pop(0)
+    card_year = events[card][0]
+    return card,card_year
 
-def one_plyaer():
+def view_card(deck):
+    return deck[0]
+
+def check_card(card, hand, events, pos):
+    """
+    uses index to chose do the ordering, 0 is first, len is last, otherwise index of the card
+    :param card:
+    :param hand:
+    :param events:
+    :param pos:
+    :return:
+    """
+    correct =  False
+    if pos == 0:
+        if hand[0][1]>= events[card][0]:
+            correct = True
+    elif pos == len(hand):
+        if hand[len(hand)-1][1] <= events[card][0]:
+            correct = True
+    else:
+        if hand[pos][1]>= events[card][0] and hand[pos-1][1] <= events[card][0]:
+            correct = True
+    return correct
+
+
+def one_player():
     print("Welcome to headlines!")
     hand = []
     events = get_events(person_csv)
     deck = shuffle_events(events)
-    hand.append(deck.pop(0))
+    hand.append(give_card(deck, events))
     print("Your current hand is:", hand)
+    current_card = view_card(deck)
+    print(current_card)
+    pos = input('Where does this card fit in your hand?')
+    int(pos)
+    if check_card(current_card,hand, events, int(pos)):
+        hand.append(give_card(deck, events))
+        print("correct")
+    print(hand)
 
 
-one_plyaer()
+
+
+
+one_player()
 
